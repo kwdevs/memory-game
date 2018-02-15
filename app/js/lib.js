101d264ef00f00e20626b2714da72d22ca853794
Kyle Morrison <morrison.devs@gmail.com>
2018-02-15 13:05:15 -0600

    feat: begun work on creating and appening random cards to gameboard.

---
+++ app/js/lib.js
@@ -0,0 +1,31 @@
+/*
+* Lib.js contains all the definitions and functions required to run the game
+*
+*
+*
+* 
+*/
+
+// Define sprite for cards front and back.
+let cardBackSprite = new Image();
+	cardBackSprite.src = 'images/playingCardBacks.png';
+
+let cardFrontSprite = new Image();
+	cardFrontSprite.src = 'images/playingCardFronts.png'
+
+
+
+
+
+
+
+
+// Constructor to create a Card Object
+// function Card () {
+// 	this.cardBack =,
+
+// 	this.cardFront = function () {
+// 		select at random from array
+// 	}
+
+// };
