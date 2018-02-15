58a16a0a78e3348eb097db5cd536b3827778a495
Kyle Morrison <morrison.devs@gmail.com>
2018-02-15 16:30:30 -0600

    feat: continued expirimentation with how I want to implement the cards.

--- app/js/lib.js
+++ app/js/lib.js
@@ -5,27 +5,69 @@
 *
 * 
 */
-
-// Define sprite for cards front and back.
-let cardBackSprite = new Image();
-	cardBackSprite.src = 'images/playingCardBacks.png';
-
-let cardFrontSprite = new Image();
-	cardFrontSprite.src = 'images/playingCardFronts.png'
+// Get the canvas element define context
+let canvas = document.getElementById('canvas');
+	ctx    = canvas.getContext('2d'); 
 
 
+// Create 16 squares for cards on canvas
+const cards = [
+	{
+		x: 25,
+		y: 25,
+		width: 125,
+		height: 150
+	},
+	{
+		x: 200,
+		y: 25,
+		width: 125,
+		height: 150
+	},
+	{
+		x: 375,
+		y: 25,
+		width: 125,
+		height: 150
+	},
+	{
+		x: 550,
+		y: 25,
+		width: 125,
+		height: 150
+	},
+	{
+		x: 25,
+		y: 185,
+		width: 125,
+		height: 150
+	},
+	{
+		x: 200,
+		y: 185,
+		width: 125,
+		height: 150
+	},
+	{
+		x: 375,
+		y: 185,
+		width: 125,
+		height: 150
+	},
+	{
+		x: 550,
+		y: 185,
+		width: 125,
+		height: 150
+	},
 
+];
 
-
-
-
-
-// Constructor to create a Card Object
-// function Card () {
-// 	this.cardBack =,
-
-// 	this.cardFront = function () {
-// 		select at random from array
-// 	}
-
-// };
+// draw cards
+cards.forEach(card => {
+	ctx.beginPath();
+	ctx.lineWidth = '2';
+	ctx.strokeStyle = 'black';
+	ctx.rect(card.x, card.y, card.width, card.height);
+	ctx.stroke();
+})
