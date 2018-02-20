f389fb6344962d56981103480333032c2f85911b
Kyle Morrison <morrison.devs@gmail.com>
2018-02-20 12:27:32 -0600

    feat: moved logic from engine to helper js for more modularity.

--- app/js/helper.js
+++ app/js/helper.js
@@ -1,56 +1,67 @@
 /*jshint esversion: 6 */
 /*The purpose of this file is to seperate out common use helper functions and hard coded data 
-* from the rest of the script
-*/
+ * from the rest of the script
+ */
 
 let frontOfCardSrc = [
-	'images/cardClubsA', 
-	'images/cardClubsJ', 
-	'images/cardClubsK', 
-	'images/cardClubsQ', 
-	'images/cardDiamondsA', 
-	'images/cardDiamondsJ', 
-	'images/cardDiamondsK', 
-	'images/cardDiamondsQ', 
-	'images/cardHeartsA', 
-	'images/cardHeartsJ', 
-	'images/cardHeartsK', 
-	'images/cardHeartsQ', 
-	'images/cardSpadesA', 
-	'images/cardSpadesJ', 
-	'images/cardSpadesK', 
-	'images/cardSpadesQ', 
+    'images/cardClubsA',
+    'images/cardClubsJ',
+    'images/cardClubsK',
+    'images/cardClubsQ',
+    'images/cardDiamondsA',
+    'images/cardDiamondsJ',
+    'images/cardDiamondsK',
+    'images/cardDiamondsQ',
+    'images/cardHeartsA',
+    'images/cardHeartsJ',
+    'images/cardHeartsK',
+    'images/cardHeartsQ',
+    'images/cardSpadesA',
+    'images/cardSpadesJ',
+    'images/cardSpadesK',
+    'images/cardSpadesQ',
 ];
 
 /* Through some research, I've decided to use the Fisher Yates Algorithm as my shuffling 
-* mechanism.
-*/
+ * mechanism.
+ */
 
-let shuffleCards = function (array) {
-	
-	let index = array.length;
-	
-	let temporaryElement = '';
+let shuffleCards = function(array) {
 
-	let randomIndex = 0;
+    let index = array.length;
 
-	while (0 !== index) {
-		
-		randomIndex = Math.floor(Math.random() * index);
+    let temporaryElement = '';
 
-		index -= 1;
+    let randomIndex = 0;
 
-		temporaryElement = array[index];
-		
-		array[index] = array[randomIndex];
-		
-		array[randomIndex] = temporaryElement;
-	}
+    while (0 !== index) {
 
-	return array;
+        randomIndex = Math.floor(Math.random() * index);
+
+        index -= 1;
+
+        temporaryElement = array[index];
+
+        array[index] = array[randomIndex];
+
+        array[randomIndex] = temporaryElement;
+    }
+
+    return array;
 };
 
-// simplify new element creation for readability.
-function newElem (elem) {
-	return document.createElement(elem);
+// simplify new element creation for readability. Accepts a string as an arguement.
+function newElem(elem) {
+    return document.createElement(elem);
+}
+
+function createDeck() {
+    // Shuffle the face card array and store it in a new array
+    let currentFaceCardArray = [];
+    currentFaceCardArray = shuffleCards(frontOfCardSrc);
+    // Create 8 objects and attach a face card image to the key frontOfCard
+    for (i = 0; i < 8; ++i) {
+        cardDeck[i] = new Card(currentFaceCardArray[i]);
+        console.log('ran');
+    }
 }
