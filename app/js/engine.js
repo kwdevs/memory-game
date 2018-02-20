ddf235f400e52efecd6253a0c5ff79884fbf8c71
Kyle Morrison <morrison.devs@gmail.com>
2018-02-19 18:07:09 -0600

    feat: changed order, how functions are called
    New variables were declared for use in Engine's lexical environment. Pseudocode updated to be more precise. Order of functions changed. New bug introduced at gameloop function.

--- app/js/engine.js
+++ app/js/engine.js
@@ -1,59 +1,77 @@
 /*
-* This is the Engine for my card matching game.  Most of the application will reside here.
-*/
+ * This is the Engine for my card matching game.  Most of the application will reside here.
+ */
 
 /* The Engine function serves as a wrapper to  create a new lexical environment which will
-* prevent collision with the global object. The function is immediately invoked and passed
-* the areguement 'this'. By doing so we will retain the ability to access the global object
-* when necessary.
-*/
-function Engine (global) {
-
-	/* Now that a new lexical environment has been created, we can declare or define variables
-	* that will be accessible throughout the entire Engine script. Like Dom selections.
-	*/
+ * prevent collision with the global object. The function is immediately invoked and passed
+ * the areguement 'this'. By doing so we will retain the ability to access the global object
+ * when necessary.
+ */
+let Engine = (function(global) {
 
 
-	/* With our variables setup, we can now move to the init function. This function is
-	* responsible for setting up the initial game when the page is loaded.  At the end, this
-	* function will kikoff the gameloop.
-	*/
+    /* Now that a new lexical environment has been created, we can declare or define variables
+     * that will be accessible throughout the entire Engine script. Like Dom selections.
+     */
+    let cardDeck = new Array,
+        doc = global.document,
+        win = global.window,
+        canvas = newElem('canvas'),
+        context = doc.getContext('2d'),
 
-	let init = function () {
+        
 
-		// The first task for the init function is to shuffle our deck.
-		
+    /* The game loop function will loop continuously using requestanimationframe(). It will also
+     * call update and render functions to continuously update the game board based on a users
+     * action.
+     */
 
-		gameLoop();
-	}();
+    function gameLoop () {
 
-	/* The game loop function will loop continuously using requestanimationframe(). It will also
-	* call update and render functions to continuously update the game board based on a users
-	* action.
-	*/
+        update();
+        render();
 
-	let gameLoop = function () {
-		
-		update();
-		render();
 
-		requestAnimationFrame(gameLoop);
-	};
+        win.requestAnimationFrame(gameLoop);
+    }
 
-	/* Now we need to define our update function. This function will mainpulate all the variables
-	* necessary to make the game functional.
-	*/
+    /* With our variables setup, we can now move to the init function. This function is
+     * responsible for setting up the initial game when the page is loaded.  At the end, this
+     * function will kickoff the gameloop.
+     */
 
-	let update = function () {
+    function init() {
 
-	};
+        // Shuffle the face card array and store it in a new array
+        let currentFaceCardArray = new Array;
+        currentFaceCardArray = shuffleCards(frontOfCardSrc);
+        // Create 8 objects and attach a face card image to the key frontOfCard
+        for (i = 0; i < 8; ++i) {
+            cardDeck[i] = new Card(currentFaceCardArray[i]);
+            console.log('ran');
+        }
 
-	/* After the necessary data has been manipulated by our update function, we use the render
-	* function to update the screen
-	*/
 
-	let render = function () {
+        gameLoop();
+    }
 
-	};
 
-}(this);
+    /* Now we need to define our update function. This function will mainpulate all the variables
+     * necessary to make the game functional.
+     */
+
+    function update() {
+    	return
+    }
+
+    /* After the necessary data has been manipulated by our update function, we use the render
+     * function to update the screen
+     */
+
+    function render() {
+    	return
+    }
+
+
+
+})(this);
