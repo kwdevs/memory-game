981ce3e0cfc0372c57cf74760ce1b4fb2331b3f4
Kyle Morrison <morrison.devs@gmail.com>
2018-02-23 13:01:44 -0600

    feat: continuing building out skeleton.

--- app/js/engine.js
+++ app/js/engine.js
@@ -8,25 +8,66 @@
  * when necessary.
  */
 let Engine = (function(global) {
-	
+	console.log('en')
 	/*First of all, we need to declare variables that will be used throughout Engines' entire scope.*/
 	let win = global;
 	let doc = global.document;
 	let currentDeck = [];
 	let winCondition = false;
 
+	function gameLoop (winCondition) {
+
+		update(winCondition);
+		draw();
+
+	}	
 
 	function init (winCondition) {
+
+		let checkForElem = doc.getElementById('container');
+			
+			if (checkForElem === null) {
+				let container = doc.newElem('div');	
+					container.id = 'container';
+					doc.appendChild(container);	
+					console.log('i exe');	
+			}
+
 		winCondition = false;
 		currentDeck = createDeck();
 		starRating  = resetStarRating();
 		moveCounter = resetMoveCounter();
+		timer = resetTimer();
+
+		gameLoop(winCondition);
 	}
 
+	function update (isWinner) {
+		
+		if (isWinner) {
+			reset();
+			return;
+		}
+
+
+	}
+
+	function draw () {
+
+
+	}
+
+	function reset () {
+
+
+
+		// calls init at the end
+		init();
+	}
 
     // kick off init when the window is ready
     win.onload = function() {
-        init();
+        init(winCondition);
     };
 
 })(this);
