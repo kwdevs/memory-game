91fc8a897dcf1cbc704b6a2d01f2675cb97731ea
Kyle Morrison <morrison.devs@gmail.com>
2018-02-27 13:16:29 -0600

    feat: function factory for tiles

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,22 @@
 /*jshint esversion: 6 */
 
 /*This file will contain object literals, function factories to create objects, and their methods*/
+
+// this is the newTile function factory
+const newTile = ( {color, tileIcon, matchingId } ) => ({
+	
+	color,
+	tileIcon,
+	matchingId,
+	
+	state: {
+		faceUp : false,
+		faceDown : true
+	},
+
+	dimensions: {
+		x : 150,
+		y : 150
+	}
+	
+});
