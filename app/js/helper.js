843315e2da46c92c36409f6f57fe5f27445f50e8
Kyle Morrison <morrison.devs@gmail.com>
2018-02-27 10:26:21 -0600

    feat: basic variables and array of FA icons declared

--- app/js/helper.js
+++ app/js/helper.js
@@ -1,3 +1,21 @@
 /*jshint esversion: 6 */
 
-/*This file contains hard coded data and all the functions we will need to perform tasks in app.js*/
+/* This file contains hard coded data and all the functions we will need to perform tasks in app.js.  'fA' or 
+'FA' is short for Font Awesome since we are using Font Awesome icons
+ */
+
+const tileColor = '#D68828FF';
+const shuffledFAArray = [];
+const currentTileDeck = [];
+
+const fAArr = [
+	'<i class="far fa-bomb"></i>',
+    '<i class="far fa-beer"></i>',
+    '<i class="far fa-bug"></i>',
+    '<i class="far fa-fighter-jet"></i>',
+    '<i class="far fa-camera-retro"></i>',
+    '<i class="far fa-fire"></i>',
+    '<i class="far fa-jack-o-lantern"></i>',
+    '<i class="far fa-space-shuttle"></i>'
+];
+
