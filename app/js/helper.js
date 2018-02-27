fdf9104b3807827aef4106839c6eb09402678e28
Kyle Morrison <morrison.devs@gmail.com>
2018-02-27 10:29:18 -0600

    feat: added shuffleCards function

--- app/js/helper.js
+++ app/js/helper.js
@@ -19,3 +19,32 @@ const fAArr = [
     '<i class="far fa-space-shuttle"></i>'
 ];
 
+
+
+/* Through some research, I've decided to use the Fisher Yates Algorithm as my shuffling 
+ * mechanism.
+ */
+
+const shuffleCards = function(array) {
+
+    let index = array.length;
+
+    let temporaryElement = '';
+
+    let randomIndex = 0;
+
+    while (0 !== index) {
+
+        randomIndex = Math.floor(Math.random() * index);
+
+        index -= 1;
+
+        temporaryElement = array[index];
+
+        array[index] = array[randomIndex];
+
+        array[randomIndex] = temporaryElement;
+    }
+
+    return array;
+};
