e75ad8cd9526ca145421d2edfdad4e61415b78a2
Kyle Morrison <morrison.devs@gmail.com>
2018-02-28 11:40:23 -0600

    feat: began work on init func to set up initial game

--- app/js/engine.js
+++ app/js/engine.js
@@ -9,10 +9,31 @@ let Engine = (function(global) {
 
 	// Variables to be used throughout Engines entire scope
 	const canvas = getCanvas();
+
+	const win = global;
 	// make canvas full screen to any device
 	canvas.width = setCanvasWidth();
 	canvas.height = setCanvasHeight();
 
 	let ctx = setCanvasContext(canvas);
 	// newDeck[0].drawRect(ctx)
+	
+	// this variable hold are array of objs that handle everything to do with the deck.
+	let newDeck = [];
+
+	// init is a function used to set the game up
+	function init() {
+		// here we set the newDeck variable to a freshly shuffled arr of icon objs
+		newDeck = iconDeck.createDeck(iconDeck.fAArr);
+		console.log("newDeck", newDeck);
+
+		//call function create tiles to draw 4x4 tiles on screen
+		drawTiles(ctx, newDeck);
+	}
+
+
+
+	// call init() as soon as the window has finished loading
+	win.onload = init();
+
 })(this);
