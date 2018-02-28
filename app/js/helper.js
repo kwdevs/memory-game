ba694dcd168815cf5b6590adc463c0a26cbe4178
Kyle Morrison <morrison.devs@gmail.com>
2018-02-28 11:09:46 -0600

    feat: simple helper functions added

--- app/js/helper.js
+++ app/js/helper.js
@@ -3,3 +3,22 @@
 /* This file will hold helper functions that do very basic tasks to keep readability of app.js and engine.js
 clean
  */
+
+// Get the canvas element
+function getCanvas() {
+    return document.querySelector('canvas');
+}
+
+function setCanvasContext(canvas) {
+	return canvas.getContext('2d');
+}
+
+// set canvas width
+function setCanvasWidth() {
+    return window.innerWidth;
+}
+
+// set canvas height
+function setCanvasHeight() {
+    return window.innerHeight;
+}
