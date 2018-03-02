6078cd830fcbe56c10e6177926a21414622fa539
Kyle Morrison <morrison.devs@gmail.com>
2018-03-02 17:14:50 -0600

    feat: creatTable fn works as expected

--- app/js/helper.js
+++ app/js/helper.js
@@ -3,3 +3,35 @@
 /* This file will hold helper functions that do very basic tasks to keep readability of app.js and engine.js
 clean
  */
+
+
+// This fn will create a table accepting row and column
+function createTable(row, col, deck) {
+
+	let currentDeck = deck;
+    let table = document.createElement('table');
+    let tableBody = document.createElement('tbody');
+
+    table.setAttribute('id', 'table');
+    tableBody.setAttribute('id', 'tableBody');
+
+    // use a nested for loop to create cells, gameboard is 4x4
+
+    for (let row = 0; row < 4; ++row) {
+
+        let newRow = table.insertRow(row);
+
+        for (let cell = 0; cell < 4; ++cell) {
+
+        	let i = 0;
+
+            let newCell = newRow.insertCell(cell);
+            	newCell.innerHTML = currentDeck[i].tileIcon;
+        	
+        	// console.log("currentDeck[i].tileIcon", currentDeck);
+            ++i;
+        }
+    }
+
+    return table;
+}
