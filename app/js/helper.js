6b43364645e30d6991501fca21f0d6bea4f9bac1
Kyle Morrison <morrison.devs@gmail.com>
2018-03-02 18:12:40 -0600

    bugfix: icons now display appropriately

--- app/js/helper.js
+++ app/js/helper.js
@@ -8,14 +8,18 @@ clean
 // This fn will create a table accepting row and column
 function createTable(row, col, deck) {
 
-	let currentDeck = deck;
+    let currentDeck = deck;
+
     let table = document.createElement('table');
     let tableBody = document.createElement('tbody');
 
     table.setAttribute('id', 'table');
     tableBody.setAttribute('id', 'tableBody');
 
-    // use a nested for loop to create cells, gameboard is 4x4
+    // use a nested for loop to create cells, gameboard is 4x4.
+    // index is declared outside the for loop to prevent it's value being reset
+    // so the arrOfObjs 'currentDeck' is fully cycled through.
+    let index = 0;
 
     for (let row = 0; row < 4; ++row) {
 
@@ -23,13 +27,13 @@ function createTable(row, col, deck) {
 
         for (let cell = 0; cell < 4; ++cell) {
 
-        	let i = 0;
+
 
             let newCell = newRow.insertCell(cell);
-            	newCell.innerHTML = currentDeck[i].tileIcon;
-        	
-        	// console.log("currentDeck[i].tileIcon", currentDeck);
-            ++i;
+            newCell.innerHTML = currentDeck[index].tileIcon;
+            console.log(index);
+          	++index;
+            // console.log("currentDeck[i].tileIcon", currentDeck);
         }
     }
 
