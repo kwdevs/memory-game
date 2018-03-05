72d3e995a08de6a3ae339c3c840ecbbda9164a93
Kyle Morrison <morrison.devs@gmail.com>
2018-03-05 14:56:20 -0600

    feat: added comments to better explain code execution. added skeletons of functions to be created.

--- app/js/helper.js
+++ app/js/helper.js
@@ -20,7 +20,7 @@ function createTable(row, col, deck) {
     // index is declared outside the for loop to prevent it's value being reset
     // so the arrOfObjs 'currentDeck' is fully cycled through.
     let index = 0;
-    
+
     //The wrapper div encapsulates the icon html to give us quick access to toggle off/on the icon display
     // let wrapperDiv = document.createElement('div');
 
@@ -30,24 +30,32 @@ function createTable(row, col, deck) {
 
         for (let cell = 0; cell < 4; ++cell) {
 
-        	// I wanted this outside the for loop but it would only append to last td created.
-        	let wrapperDiv = document.createElement('div');
-        	wrapperDiv.innerHTML = currentDeck[index].tileIcon;
-        	// add id
-        	wrapperDiv.id = index;
+            // I wanted this outside the for loop but it would only append to last td created.
+            let wrapperDiv = document.createElement('div');
+            wrapperDiv.innerHTML = currentDeck[index].tileIcon;
+            // add id
+            wrapperDiv.id = index;
             // add display none by default
             wrapperDiv.style.display = 'none';
+            // create a new cell and append wrapper div containing FA icon.
             let newCell = newRow.insertCell(cell);
             newCell.appendChild(wrapperDiv);
-            
-            // newCell.innerHTML = currentDeck[index].tileIcon;
             console.log("newCell", newCell);
-            
-          	++index;
-            // console.log("currentDeck[i].tileIcon", currentDeck);
+
+            ++index;
         }
     }
 
 
     return table;
+}
+
+// this function will listen for clicks on the able and set the correct td's parent div to display the icon
+function flipTile(event) {
+
+}
+
+// function addListeners is used to kick off all necessary event listeners in the init stage of engine.js
+function addListeners() {
+	
 }
